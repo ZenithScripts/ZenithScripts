9e6cc5e2db69b36470f78e6aa06e566413b9938a
Rolao <105136790+dysyfsd@users.noreply.github.com>
2024-06-02 16:39:37 -0400

    Update Hub1.lua

--- Hub1.lua
+++ Hub1.lua
@@ -1,31 +1,77 @@
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 
-local Window = OrionLib:MakeWindow({Name = "Apocalypse Rising 2", HidePremium = true, SaveConfig = true, ConfigFolder = "MyHubConfig"})
+local Window = OrionLib:MakeWindow({Name = "", HidePremium = true, SaveConfig = true, ConfigFolder = "MyHubConfig"})
 
 local Tab1 = Window:MakeTab({
+    Name = "Phantom Forces",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local Tab2 = Window:MakeTab({
     Name = "Apocalypse Rising 2",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
+local Tab3 = Window:MakeTab({
+    Name = "Counter Blox",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local Tab4 = Window:MakeTab({
+    Name = "Arsenal",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
 Tab1:AddButton({
+    Name = "Hub PF2 Antigo o mior",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/dysyfsd/My/main/Hub2.lua"))()
+    end    
+})
+
+Tab1:AddButton({
+    Name = "Hub PF2 Novo",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/dysyfsd/My/main/Roludao.lua"))()
+    end    
+})
+
+Tab2:AddButton({
     Name = "ESP AR2",
     Callback = function()
         loadstring(game:HttpGet("https://pastebin.com/raw/YbLrHQDB"))()
     end    
 })
 
-Tab1:AddButton({
+Tab2:AddButton({
     Name = "FullBright (L Key)",
     Callback = function()
         loadstring(game:HttpGet("https://pastebin.com/raw/SRAgYGRz"))()
     end    
 })
 
-Tab1:AddButton({
+Tab2:AddButton({
     Name = "Infinite Yield",
     Callback = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    end    
+})
+
+Tab3:AddButton({
+    Name = "Hub CB:RO",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fliskScript/jaran.vip/main/free.lua"))()
+    end    
+})
+
+Tab4:AddButton({
+    Name = "HUB ARSENAL",
+    Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/wuhaz/arsenal/main/main.lua')))() 
     end    
 })
 
